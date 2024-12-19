86d265b4587748704d27ca316f1a413f840ecc56
issac <issacthomasvarghese@gmail.com>
2024-12-19 22:50:15 +0530

    update:install script

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
-#!/bin/bash
-url="https://github.com/zaac04/lockenv/releases/download/v2.1/lockenv"
-wget -O lockenv $url
-binary_destination_folder="/usr/local/bin"
-sudo cp lockenv $binary_destination_folder
-sudo chmod +x "$binary_destination_folder/lockenv"
+#!/bin/sh
+LOCKENV_VERSION=$1
+EDITION=$2
+LOCKENV_URL="https://github.com/zaac04/lockenv/releases/download/${LOCKENV_VERSION}/${EDITION}"
+BINARY_DESTINATION_FOLDER="/usr/local/bin"
+wget -O lockenv $LOCKENV_URL && \
+cp lockenv $BINARY_DESTINATION_FOLDER && \
+chmod +x "$BINARY_DESTINATION_FOLDER/lockenv" && \
+rm lockenv
